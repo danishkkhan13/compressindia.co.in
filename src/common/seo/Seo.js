86bf06bead4001446cec55e2307f8de35de5aef8
Danish Khan <danish@dynsimulation.com>
2025-05-27 12:33:58 +0530

    minor changes

--- src/common/seo/Seo.js
+++ src/common/seo/Seo.js
@@ -1,17 +1,18 @@
 import Head from "next/head";
 
-const SEO = ({ pageTitle }) => (
-    <>
-        <Head>
-            <title>
-                {pageTitle && `${pageTitle} || Compress || AC Services React Next JS Template`}
-            </title>
-            <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-            <meta name="description" content="Compress AC Services React Next JS Template" />
-            <meta name="robots" content="noindex, follow" />
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        </Head>
-    </>
+/**
+ * @param {{ pageTitle: string, description?: string }} props
+ */
+const SEO = ({ pageTitle, description }) => (
+  <>
+    <Head>
+      <title>{pageTitle}</title>
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta name="description" content={description } />
+      <meta name="robots" content="index, follow" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    </Head>
+  </>
 );
 
 export default SEO;
